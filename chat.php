841aa8358cdb75f41142db180fe2c51c31593cbd
Aryan Ahmed Anik <thearyanahmed@gmail.com>
2018-02-26 23:34:14 +0600

    added subscribe

--- chat.php
+++ chat.php
@@ -14,3 +14,37 @@ $pubnub = new Pubnub(
     "sec-c-NTQ3MmFjZTQtYmVmNy00MGI4LWFkMGMtZjdlZWYwM2NkNTJk",   
     false    ## SSL_ON?
 );
+
+$connectAs = function() {
+
+	fwrite(STDOUT, 'Connect as : ');
+	
+	return trim(fgets(STDIN));
+};
+
+
+fwrite(STDOUT, 'Join room : ');
+
+$room = trim(fgets(STDIN));
+
+$username = $connectAs();
+
+fwrite(STDOUT, "\n Connected to '{$room}' as '{$username}' \n");
+
+$pid = pcntl_fork();
+
+if ($pid == -1) {
+
+	exit(1);
+} else if ($pid) {
+
+
+	pcntl_wait($status);
+} else {
+
+
+	$pubnub->subscribe($room , function($payload) {
+	    var_dump($payload);  
+	    return true;         
+	});
+}
