57b5aadb5e74003bfa5773333b957aec21b5afd5
prophecy <thearyanahmed@gmail.com>
2021-04-06 00:24:51 +0600

    Update chat.php

--- chat.php
+++ chat.php
@@ -9,9 +9,9 @@ use Pubnub\Pubnub;
 
 
 $pubnub = new Pubnub(
-    "pub-c-a658d87b-d656-445e-94d3-5e5edeaa3bf9",  
-    "sub-c-9b4155ec-1af5-11e8-acdc-3a42756f9040",  
-    "sec-c-NTQ3MmFjZTQtYmVmNy00MGI4LWFkMGMtZjdlZWYwM2NkNTJk",   
+    "pubnub-public-key",  
+    "pubnub-subscriber-key-ithnk",  
+    "pubnub-secret-key-maybe?",   
     false   
 );
 
